b92fcd3c5aa38a175fdedb0b512f13b9bdb19822
dlamli <dannylamli@hotmail.com>
2024-12-17 16:19:54 -0600

    feat:Disable button conditionally

--- src/components/GameBoardColumn.tsx
+++ src/components/GameBoardColumn.tsx
@@ -8,7 +8,7 @@ const GameBoardColumn = ({
 }: GameBoardColumnProps) => {
   return (
     <li>
-      <button onClick={() => onClick(rowIndex, colIndex)}>
+      <button onClick={() => onClick(rowIndex, colIndex)} disabled={playerSymbol !== null}>
         {playerSymbol}
       </button>
     </li>
